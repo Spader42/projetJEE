ec564ed7d721e5983242829826afd757d9bd309a
Spader42 <steevenbrest@hotmail.com>
2018-01-11 16:57:55 +0100

    SDD - Ajout du controlleur météo

--- src/controller/MeteoController.java
+++ src/controller/MeteoController.java
@@ -1,5 +1,25 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import dao.Dao;
+
+@Controller
 public class MeteoController {
 
+	@Autowired
+	Dao dao;
+	
+	public MeteoController() {
+		
+	}
+	
+	@RequestMapping(value="/consulterMeteo",method=RequestMethod.POST)
+	public @ResponseBody String consulterMeteo() {		
+		return "consulterMeteo";
+	}
 }
