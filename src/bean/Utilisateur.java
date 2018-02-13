12ba70ce6d262bd370546af7c257d93704ab700e
Spader42 <steevenbrest@hotmail.com>
2018-02-13 14:51:11 +0100

    SDD : Modification de l'objet utilisateur post-erreur

--- src/bean/Utilisateur.java
+++ src/bean/Utilisateur.java
@@ -77,30 +77,3 @@ public class Utilisateur {
 		this.sel = sel;
 	}
 }
-package bean;
-
-import javax.validation.constraints.Size;
-
-import org.hibernate.validator.constraints.NotEmpty;
-
-public class Utilisateur {
-
-	@Size(min=5, message="Le pseudo doit avoir au moins 5 caractères")
-	String login;
-
-	@Size(min=5, max=15, message="Le mot de passe doit faire entre 5 et 15 caractères")
-	String motDePasse;
-	
-	public String getLogin() {
-		return login;
-	}
-	public void setLogin(String login) {
-		this.login = login;
-	}
-	public String getMotDePasse() {
-		return motDePasse;
-	}
-	public void setMotDePasse(String motDePasse) {
-		this.motDePasse = motDePasse;
-	}
-}
